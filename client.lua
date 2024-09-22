83bc2eb882138cb8f4c583af15b4aa50d903935b
DrD <78091157+otavioomattos@users.noreply.github.com>
2024-09-22 12:23:46 -0300

    New event added client.lua

--- client.lua
+++ client.lua
@@ -2,3 +2,8 @@ RegisterNetEvent("vorp_inventory:OpenInventory", function(invId, options)
     TriggerServerEvent("inventory:server:OpenInventory", "stash", invId, options)
     TriggerEvent("inventory:client:SetCurrentStash", invId)
 end)
+
+RegisterNetEvent("vorp_inventory:OpenstealInventory", function(title, invID)
+    title = "otherplayer"
+    TriggerServerEvent("inventory:server:OpenInventory", title, invID)
+end)
